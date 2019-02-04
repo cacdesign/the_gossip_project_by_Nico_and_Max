ea3d70bb81b0148032f12498c19a31286f57beaa
Laspargus <maxime.speroni@gmail.com>
2019-02-04 14:34:42 +0100

    hidden url

---
+++ app/controllers/gossip_controller.rb
@@ -0,0 +1,13 @@
+class GossipController < ApplicationController
+  def team
+  end
+def contact
+end
+
+def welcome
+  @first_name=params[:first_name]
+  puts '€'*60
+  puts @first_name
+  puts '€'*60
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'gossip/team'
+  get 'gossip/contact'
+  get 'welcome/:first_name', to: 'gossip#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
