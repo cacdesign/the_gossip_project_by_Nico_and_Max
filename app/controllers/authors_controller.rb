587b139fcc784e845ce32127920f58b52351a1fd
cacdesign <pro.nicolasfernandez@gmail.com>
2019-02-07 12:00:52 +0100

    ok

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -16,7 +16,7 @@ end
 
 	@city = City.create(name: params["user_city"], zip_code: params["user_zip_code"])	
 	puts "$" *24
-	puts @user = User.new(first_name: params["user_first_name"], last_name: params["user_last_name"], email:params["user_email"], age: params["user_age"], password: params["user_password"], city: @city, password: params["user_description"])
+	puts @user = User.new(first_name: params["user_first_name"], last_name: params["user_last_name"], email:params["user_email"], age: params["user_age"], password: params["user_password"], city: @city, description: params["user_description"])
 	#@user = User.new(post_params)
 	puts "$" *24
 		if @user.save
