5d6143107432bb8172967149a4772ee09539d598
cacdesign <pro.nicolasfernandez@gmail.com>
2019-02-07 10:41:52 +0100

    connexion

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,38 @@ class AuthorsController < ApplicationController
     @author=User.find(@id)
     @city_id = @author.city.id
 end
+
+	def new
+
+	end
+
+	def create
+
+
+	#post_params = params.permit(:user_city, :user_zip_code, :user_first_name, :user_last_name, :user_email, :user_age)
+
+	@city = City.create(name: params["user_city"], zip_code: params["user_zip_code"])	
+	puts "$" *24
+	puts @user = User.new(first_name: params["user_first_name"], last_name: params["user_last_name"], email:params["user_email"], age: params["user_age"], password: params["user_password"], city: @city, password: params["user_description"])
+	#@user = User.new(post_params)
+	puts "$" *24
+		if @user.save
+			flash[:success] = "Well done MF"
+			puts @user.errors
+			redirect_to gossips_path
+		else
+			puts @user.errors
+			render 'new'
+		end
+
+	end
+
+private
+
+	def details
+
+	end
+
+
 end
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ belongs_to :city
 has_many :sent_messages, foreign_key: 'sender_id', class_name: 'PrivateMessage'
 has_many :received_messages, foreign_key: 'receiver_id', class_name: 'PrivateMessage'
 has_many :comments
+has_secure_password
 
 validates :first_name, presence: true
 validates :last_name, presence: true
